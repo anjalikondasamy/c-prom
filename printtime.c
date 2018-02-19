d3ab8fdd1c5e9bc6acd945962f6b88ebb67be19c
anjalikondasamy <35995743+anjalikondasamy@users.noreply.github.com>
2018-02-19 16:07:34 +0530

    c pgm
    good

---
+++ printtime.c
@@ -0,0 +1,14 @@
+#include,stdio.h>
+#include<conio.h>
+void main()
+{
+int minutes,hours;
+clrscr();
+
+printf("\n Enter the minutes");
+scaanf("%d",&minutes);
+
+printf("\n Enter the hours");
+scanf"%d"&hours");
+
+}
